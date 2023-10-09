5d185695dddfd63c38ecb4f39bd84aea9fb13966
Soubinan <contact@soubinan.tk>
2023-10-09 02:20:54 -0400

    update cloudflare provider version

--- cloudflare/settings.tf
+++ cloudflare/settings.tf
@@ -1,12 +1,24 @@
-resource "cloudflare_zone_settings_override" "soubilabs_https" {
+resource "cloudflare_zone_settings_override" "soubilabs_zone_settings" {
   zone_id = var.soubilabs_zone_id
+
   settings {
     always_use_https         = "on"
     brotli                   = "on"
     tls_1_3                  = "on"
     automatic_https_rewrites = "on"
     ssl                      = "strict"
-    waf                      = "on"
+
+    http2 = null
+    mirage = null
+    origin_error_page_pass_thru = null
+    polish = null
+    prefetch_preload = null
+    proxy_read_timeout = null
+    response_buffering = null
+    sort_query_string_for_cache = null
+    true_client_ip_header = null
+    webp = null
+    image_resizing = null
 
     minify {
       css  = "on"
@@ -15,12 +27,29 @@ resource "cloudflare_zone_settings_override" "soubilabs_https" {
     }
 
     security_header {
-      enabled = true
+      enabled            = true
       include_subdomains = true
     }
   }
 }
 
+resource "cloudflare_zone_settings_override" "soubilabs_https" {
+  zone_id = var.soubilabs_zone_id
+
+  settings {
+    http2 = null
+    mirage = null
+    origin_error_page_pass_thru = null
+    polish = null
+    prefetch_preload = null
+    proxy_read_timeout = null
+    response_buffering = null
+    sort_query_string_for_cache = null
+    true_client_ip_header = null
+    webp = null
+    image_resizing = null
+  }
+}
 resource "cloudflare_bot_management" "soubilabs_anti_bot" {
   zone_id    = var.soubilabs_zone_id
   enable_js  = true
