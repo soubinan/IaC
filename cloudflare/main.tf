1912856e259499076cce21196f2a79043190666e
Soubinan <contact@soubinan.tk>
2023-10-09 01:06:00 -0400

    update var name and add https configs

--- cloudflare/main.tf
+++ cloudflare/main.tf
@@ -1,13 +1,25 @@
-resource "cloudflare_record" "blog" {
-  zone_id = var.zone_id
+resource "cloudflare_record" "soubilabs_blog" {
+  zone_id = var.soubilabs_zone_id
   name    = "blog"
   value   = "hashnode.network"
   type    = "CNAME"
   proxied = false
 }
 
-resource "cloudflare_bot_management" "anti_bot" {
-  zone_id    = var.zone_id
+resource "cloudflare_zone_settings_override" "soubilabs_https" {
+  zone_id = var.soubilabs_zone_id
+  settings {
+    always_use_https         = "on"
+    brotli                   = "on"
+    tls_1_3                  = "on"
+    automatic_https_rewrites = "on"
+    ssl                      = "strict"
+    waf                      = "on"
+  }
+}
+
+resource "cloudflare_bot_management" "soubilabs_anti_bot" {
+  zone_id    = var.soubilabs_zone_id
   enable_js  = true
   fight_mode = true
 }
--- cloudflare/variables.tf
+++ cloudflare/variables.tf
@@ -1,7 +1,12 @@
-variable "domain" {
+variable "soubilabs_domain" {
   default = "soubinan.xyz"
 }
 
-variable "zone_id" {
+variable "soubilabs_zone_id" {
   default = "4c0aeed8407334470ead0551c57e3cd3"
+}
+
+variable "soubilabs_account_id" {
+  type    = string
+  default = "f2dda9cf92c02eb24ced3b63332b6055"
 }
