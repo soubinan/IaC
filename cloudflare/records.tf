27e9dcd9d042b5ce0c68dffcedc0575fd524700e
Soubinan <contact@soubinan.tk>
2023-10-09 18:21:26 -0400

    update dmarc reccord

--- cloudflare/records.tf
+++ cloudflare/records.tf
@@ -25,7 +25,7 @@ resource "cloudflare_record" "soubilabs_apex" {
 resource "cloudflare_record" "soubilabs_dmarc" {
   zone_id = var.soubilabs_zone_id
   name    = "_dmarc"
-  value   = "v=DMARC1;p=none;rua=mailto:a1b8e51de9@rua.easydmarc.us;ruf=mailto:a1b8e51de9@ruf.easydmarc.us;fo=1;"
+  value   = "v=DMARC1;p=reject;pct=100;rua=mailto:a1b8e51de9@rua.easydmarc.us;ruf=mailto:a1b8e51de9@ruf.easydmarc.us;ri=86400;aspf=s;adkim=s;fo=1"
   type    = "TXT"
 }
 
