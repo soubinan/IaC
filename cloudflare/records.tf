0c70ddb5faf5ecadce8c77527751e7b6f5768929
Soubinan <contact@soubinan.tk>
2023-10-09 17:16:24 -0400

    update mta-sts reccord

--- cloudflare/records.tf
+++ cloudflare/records.tf
@@ -69,7 +69,7 @@ resource "cloudflare_record" "soubilabs_dkim" {
 
 resource "cloudflare_record" "soubilabs_mtasts" {
   zone_id = var.soubilabs_zone_id
-  name    = "@"
+  name    = "MTA-STS"
   value   = "v=STSv1; id=169688529676Z;"
   type    = "TXT"
 }
