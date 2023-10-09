e949910fec709e6a10a90201192ce5facc6673a7
Soubinan <contact@soubinan.tk>
2023-10-09 15:50:56 -0400

    cleanup zone settings - 5

--- cloudflare/settings.tf
+++ cloudflare/settings.tf
@@ -1,50 +1,15 @@
-# resource "cloudflare_zone_settings_override" "soubilabs_zone_settings" {
-#   zone_id = var.soubilabs_zone_id
-
-#   settings {
-#     always_use_https         = "on"
-#     brotli                   = "on"
-#     tls_1_3                  = "on"
-#     automatic_https_rewrites = "on"
-#     ssl                      = "strict"
-
-#     http2                       = null
-#     mirage                      = null
-#     origin_error_page_pass_thru = null
-#     polish                      = null
-#     prefetch_preload            = null
-#     proxy_read_timeout          = null
-#     response_buffering          = null
-#     sort_query_string_for_cache = null
-#     true_client_ip_header       = null
-#     webp                        = null
-#     image_resizing              = null
-
-#     minify {
-#       css  = "on"
-#       js   = "on"
-#       html = "on"
-#     }
-
-#     security_header {
-#       enabled            = true
-#       include_subdomains = true
-#       max_age            = 31536000
-#     }
-#   }
-# }
-
-# resource "cloudflare_bot_management" "soubilabs_anti_bot" {
-#   zone_id    = var.soubilabs_zone_id
-#   enable_js  = true
-#   fight_mode = true
-# }
-
-
-resource "cloudflare_zone_settings_override" "soubilabs_https" {
+resource "cloudflare_zone_settings_override" "soubilabs_zone_settings" {
   zone_id = var.soubilabs_zone_id
 
   settings {
+    waf                      = "on"
+    always_use_https         = "on"
+    brotli                   = "on"
+    tls_1_3                  = "on"
+    automatic_https_rewrites = "on"
+    ssl                      = "strict"
+    min_tls_version          = "1.2"
+
     http2                       = null
     mirage                      = null
     origin_error_page_pass_thru = null
@@ -56,8 +21,6 @@ resource "cloudflare_zone_settings_override" "soubilabs_https" {
     true_client_ip_header       = null
     webp                        = null
     image_resizing              = null
-
-    ssl                      = "strict"
 
     minify {
       css  = "on"
@@ -71,4 +34,10 @@ resource "cloudflare_zone_settings_override" "soubilabs_https" {
       max_age            = 31536000
     }
   }
+}
+
+resource "cloudflare_bot_management" "soubilabs_anti_bot" {
+  zone_id    = var.soubilabs_zone_id
+  enable_js  = true
+  fight_mode = true
 }
