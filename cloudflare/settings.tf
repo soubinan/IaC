55b755c767bf9e29376a4717589f7520e9a790a6
Soubinan <contact@soubinan.tk>
2023-10-09 13:41:51 -0400

    cleanup zone settings - mitigate tainted ressource - 2

--- cloudflare/settings.tf
+++ cloudflare/settings.tf
@@ -45,6 +45,18 @@ resource "cloudflare_zone_settings_override" "soubilabs_https" {
   zone_id = var.soubilabs_zone_id
 
   settings {
+    http2                       = null
+    mirage                      = null
+    origin_error_page_pass_thru = null
+    polish                      = null
+    prefetch_preload            = null
+    proxy_read_timeout          = null
+    response_buffering          = null
+    sort_query_string_for_cache = null
+    true_client_ip_header       = null
+    webp                        = null
+    image_resizing              = null
+
     ssl                      = "strict"
 
     minify {
