35bb194f2e693657651a4149ef8089fe0d425946
Soubinan <contact@soubinan.tk>
2023-10-09 02:06:05 -0400

    update apex records

--- cloudflare/records.tf
+++ cloudflare/records.tf
@@ -8,8 +8,16 @@ resource "cloudflare_record" "soubilabs_blog" {
 
 resource "cloudflare_record" "soubilabs_site" {
   zone_id = var.soubilabs_zone_id
-  name    = "@"
+  name    = "www"
   value   = "soubinan.github.io"
   type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "soubilabs_site" {
+  zone_id = var.soubilabs_zone_id
+  name    = "@"
+  value   = "soubinan.github.io"
+  type    = "A"
   proxied = false
 }
