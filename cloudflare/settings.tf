6a14cbe6f193256f33627b9d7d0fb8a1ad086715
Soubinan <contact@soubinan.tk>
2023-10-10 00:21:40 -0400

    add mta-sts worker - fix binding

--- cloudflare/settings.tf
+++ cloudflare/settings.tf
@@ -45,21 +45,27 @@ resource "cloudflare_bot_management" "soubilabs_anti_bot" {
   fight_mode = true
 }
 
-resource "cloudflare_worker_domain" "mta_sts" {
+resource "cloudflare_worker_domain" "soubilabs_mta_sts" {
   account_id = var.soubilabs_account_id
   zone_id    = var.soubilabs_zone_id
   hostname   = "mta-sts.${var.soubilabs_domain}"
-  service    = "mta-sts"
+  service    = "mta_sts_service"
+}
+
+resource "cloudflare_worker_route" "soubilabs_mta_sts_route" {
+  zone_id    = var.soubilabs_zone_id
+  pattern     = "mta-sts.${var.soubilabs_domain}/*"
+  script_name = cloudflare_worker_script.mta_sts_script.name
 }
 
 resource "cloudflare_worker_script" "mta_sts_script" {
   account_id = var.soubilabs_account_id
-  name       = "mta-sts"
+  name       = "mta_sts_script"
   content    = file("${path.module}/mta-sts.js")
 
   service_binding {
-    name        = "mta-sts_service"
-    service     = cloudflare_worker_domain.mta_sts.service
+    name        = "mta_sts"
+    service     = cloudflare_worker_domain.soubilabs_mta_sts.service
     environment = "production"
   }
 }
