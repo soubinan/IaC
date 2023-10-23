152f875e59145edc1cd424f7fc779ce17ccdba10
Soubinan <contact@soubinan.tk>
2023-10-22 22:34:07 -0400

    add labs subdomain and subdomains wildcards

--- cloudflare/records.tf
+++ cloudflare/records.tf
@@ -6,6 +6,30 @@ resource "cloudflare_record" "soubilabs_blog" {
   proxied = false
 }
 
+resource "cloudflare_record" "soubilabs_blog" {
+  zone_id = var.soubilabs_zone_id
+  name    = "*.blog"
+  value   = "hashnode.network"
+  type    = "CNAME"
+  proxied = false
+}
+
+resource "cloudflare_record" "soubilabs_www" {
+  zone_id = var.soubilabs_zone_id
+  name    = "labs"
+  value   = "soubinan.github.io"
+  type    = "CNAME"
+  proxied = true
+}
+
+resource "cloudflare_record" "soubilabs_www" {
+  zone_id = var.soubilabs_zone_id
+  name    = "*.labs"
+  value   = "soubinan.github.io"
+  type    = "CNAME"
+  proxied = true
+}
+
 resource "cloudflare_record" "soubilabs_www" {
   zone_id = var.soubilabs_zone_id
   name    = "www"
