8db9b25b8b72b51858a89d9fec1b790774538770
Soubinan <contact@soubinan.tk>
2025-01-26 01:12:20 -0500

    update lxc module: update outputs

--- modules/lxc/outputs.tf
+++ modules/lxc/outputs.tf
@@ -1,7 +1,11 @@
 output "ansible_stdout" {
-  value = ansible_playbook.configure_lxc_machine.*.ansible_playbook_stdout
+  value = ansible_playbook.configure_lxc_machine[*].ansible_playbook_stdout
 }
 
 output "ansible_stderr" {
-  value = ansible_playbook.configure_lxc_machine.*.ansible_playbook_stderr
+  value = ansible_playbook.configure_lxc_machine[*].ansible_playbook_stderr
+}
+
+output "ip_addresses" {
+  value = var.ip_configs[*].ip
 }
