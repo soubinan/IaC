e752e6b284e00df542ae34c7503036a0bbdf6e4e
Soubinan <contact@soubinan.tk>
2025-03-22 17:22:36 -0400

    Update OIDC client configuration to use basic authentication and modify credential structure

--- zitadel-admin/client_sno.tf
+++ zitadel-admin/client_sno.tf
@@ -6,7 +6,7 @@ resource "zitadel_application_oidc" "sno" {
   app_type                     = "OIDC_APP_TYPE_USER_AGENT"
   response_types               = ["OIDC_RESPONSE_TYPE_CODE"]
   grant_types                  = ["OIDC_GRANT_TYPE_AUTHORIZATION_CODE"]
-  auth_method_type             = "OIDC_AUTH_METHOD_TYPE_NONE"
+  auth_method_type             = "OIDC_AUTH_METHOD_TYPE_BASIC"
   version                      = "OIDC_VERSION_1_0"
   redirect_uris                = ["https://oauth-openshift.apps.okd.lab.soubilabs.xyz/oauth2callback/zitadel"]
   post_logout_redirect_uris    = ["https://console-openshift-console.apps.okd.lab.soubilabs.xyz"]
@@ -20,17 +20,17 @@ resource "zitadel_application_oidc" "sno" {
   skip_native_app_success_page = true
 }
 
-resource "random_string" "sno_client_secret_hash" {
-  length = 16
-  keepers = {
-    uuid = "${uuid()}"
-  }
-}
+# resource "random_string" "sno_client_secret_hash" {
+#   length = 16
+#   keepers = {
+#     uuid = "${uuid()}"
+#   }
+# }
 
 resource "local_sensitive_file" "sno_credentials" {
   content = jsonencode({
-    "client_id" : zitadel_application_oidc.sno.client_id,
-    "client_secret" : random_string.sno_client_secret_hash.result,
+    "clientId" : zitadel_application_oidc.sno.client_id,
+    "clientSecret" : zitadel_application_oidc.sno.client_secret,
   })
   filename = "${path.module}/.creds.sno_oidc.json"
 
